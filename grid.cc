591a6be46b1594feb5fd9f1ac76ef222f9d9a553
Junyi Chai <junyic@princeton.edu>
2014-11-16 19:53:00 -0500

    Implement a smart grid class

---
+++ grid.cc
@@ -0,0 +1,78 @@
+#include"grid.h"
+#include<stdexcept>
+#include<cmath>
+
+static const double kPI = 3.14159265359;
+
+
+Grid::Grid(int nx, int ny):
+    nx_(nx), ny_(ny){
+    if(nx < 2 || ny < 2)
+        throw std::invalid_argument("Domain must be at least 2x2");
+
+    double * field2d_ = new double[nx*ny];
+    InitializeBoundary();
+}
+
+Grid::Grid(int n): 
+    nx_(n), ny_(n){
+    if(n < 2)
+        throw std::invalid_argument("Domain must be at least 2x2");
+
+    double * field2d_ = new double[n*n];
+    InitializeBoundary();
+}
+
+Grid::~Grid(){
+    delete [] field2d_;
+}
+
+void Grid::DomainToSolve(int & min_x, int & max_x,
+                   int & min_y, int & max_y){
+    min_x = 0;
+    max_x = nx_ - 2; ///no need to solve the right boundary as it's the same
+                     ///as the left boundary (periodic condition in x)
+    min_y = 1; /// y = 0 and y = ny_-1 are fixed
+    max_y = ny_ - 2;
+}
+
+void Grid::InitializeBoundary(){
+    for(int ix = 0; ix < nx_; ix++){
+        double x = double(ix) / double(nx_-1) * kPI;
+        double sin2_x = std::sin(x) * std::sin(x);
+        field2d_[ix * ny_]         = 1.0 - sin2_x; ///lower boundary
+        field2d_[ny_-1 + ix * ny_] = sin2_x;       ///upper boundary
+    }
+}
+
+/// the responsibility that ix, and jy are valid lie on the user
+double Grid::get_point(int ix, int jy) const{
+    Grid * local_this = const_cast<Grid * >(this);
+    double point_ij = local_this->set_point(ix, jy);
+    return point_ij;
+}
+
+double & Grid::set_point(int ix, int jy){
+    if(ix == 0)        return get_left_boundary(jy);
+    if(ix == nx_ -1 )  return get_right_boundary(jy);
+    if(jy == 0)        return get_lower_boundary(ix);
+    if(jy == ny_ -1 )  return get_upper_boundary(ix);
+
+    return field2d_[jy + ix * ny_];
+}   
+
+double & Grid::get_left_boundary(int jy){
+    return field2d_[jy];
+}
+
+double & Grid::get_right_boundary(int jy){
+    return field2d_[jy + (nx_-1)*ny_];
+}
+
+double & Grid::get_lower_boundary(int ix){
+    return field2d_[ix*ny_];
+}
+
+double & Grid::get_upper_boundary(int ix){
+    return field2d_[ny_-1 + ix * ny_];
+}
---
+++ grid.h
@@ -0,0 +1,60 @@
+#ifndef _SMART_GRID_H
+#define _SMART_GRID_H
+
+/**
+ * @brief grid which stores the field to work on and automatically
+ *        handles the boundary condition
+ *
+ * specifically handles periodic boundary condition in x direction,
+ * that is T(0,y) = T(pi,y)
+ * and T(x,0) = cos^2(x), T(x,pi) = sin^2(x)
+ * indexing follows the C notation that first element has index 0
+ */
+
+class Grid{
+public:
+    ///construct a nx x ny 2 dimensnional field
+    Grid(int nx, int ny);
+
+    ///construct a n x n 2 dimensional field
+    Grid(int n);
+
+    ~Grid();
+
+    ///set the constant boundary condtion at y = 0 and y = pi
+    void InitializeBoundary();
+
+    ///returns the index range for the domain to solve, which is
+    /// [min_x, max_x] x [min_y, max_y]
+    void DomainToSolve(int & min_x, int & max_x,
+                       int & min_y, int & max_y);
+
+    ///returns the value at point (ix, jy)
+    inline double get_point(int ix, int jy) const;
+
+    ///returns the reference for point (ix, jy) so that one can set
+    ///the point as set_point(ix,jy) = some_value;
+    inline double & set_point(int ix, int jy);
+
+
+private:
+    /**
+     * to store 2d field; implemented as an 1d array
+     * indexing is faster in y direction, so
+     * field2d_(ix, jy) = field2d_[jy + ix * ny_]
+    */
+    double * field2d_;    
+    const int nx_;    ///< number of grids in x direction
+    const int ny_;    ///< number of grids in y direction
+
+    inline double & get_left_boundary (int jy);
+    inline double & get_right_boundary(int jy);
+    inline double & get_lower_boundary(int ix);
+    inline double & get_upper_boundary(int ix);
+
+    ///hide copy constructor and copy assigment operator
+    Grid(Grid& );
+    Grid& operator=(const Grid& );
+};
+
+#endif //_SMART_GRID_H
